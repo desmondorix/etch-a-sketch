45a001101f6957d43886c6ac5cc1a6353b5aa9f6
desmondorix <desmondorixk@gmail.com>
2025-07-30 20:18:20 +0700

    done random rgb and darkening

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function gridMaker(size) {
   for (var i = 0; i < size * size; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
+    box.dataset.hover = 0;
     box.style.width = `calc(100% / ${size})`;
     box.style.height = `calc(100% / ${size})`;
     container.appendChild(box);
@@ -28,5 +29,30 @@ function gridMaker(size) {
 }
 
 var changeColor = (box) => {
-  box.style.backgroundColor = "black";
+  let countHover = parseInt(box.dataset.hover);
+
+  // Jika pertama kali di-hover
+  if (countHover === 0) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    box.dataset.rgb = `${r},${g},${b}`;
+    box.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+    box.dataset.hover = 1;
+  }
+
+  // Jika sudah pernah dihover dan kurang dari 10x
+  else if (countHover < 10) {
+    const [r, g, b] = box.dataset.rgb.split(",").map(Number);
+    const factor = 1 - countHover * 0.1;
+
+    const darkR = Math.floor(r * factor);
+    const darkG = Math.floor(g * factor);
+    const darkB = Math.floor(b * factor);
+
+    box.style.backgroundColor = `rgb(${darkR},${darkG},${darkB})`;
+    box.dataset.hover = countHover + 1;
+  }
 };
